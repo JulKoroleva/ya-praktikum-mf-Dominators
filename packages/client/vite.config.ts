cd6707d5e0143706e180e38236f62d199cd6aeaf
Jkoroleva <ykoroleva@t-argos.ru>
2024-11-20 16:30:14 +0300

    feat(GAME-3): Update vite config

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "src/styles/variables.scss";`,
+        additionalData: `@use "src/styles/variables" as *;`,
       },
     },
   },
