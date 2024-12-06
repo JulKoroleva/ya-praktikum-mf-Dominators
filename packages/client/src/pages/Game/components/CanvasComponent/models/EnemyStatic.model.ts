ed5f23c2675feb3a242a958e36892de3bd216558
Jkoroleva <ykoroleva@t-argos.ru>
2024-12-06 15:07:29 +0300

    feat(GAME-23): Create virus canvas appearance

--- packages/client/src/pages/Game/components/CanvasComponent/models/EnemyStatic.model.ts
+++ packages/client/src/pages/Game/components/CanvasComponent/models/EnemyStatic.model.ts
@@ -24,4 +24,68 @@ export class EnemyStatic extends GameFeatureModel {
     this.ToX = this.X + Math.cos(this.Angle) * 20;
     this.ToY = this.Y + Math.sin(this.Angle) * 20;
   }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    // Наружные шипы
+    const spikeCount = 10;
+    const spikeLength = this.Radius * 0.3;
+    const baseWidth = this.Radius * 0.2;
+    for (let i = 0; i < spikeCount; i++) {
+      const angle = (i / spikeCount) * Math.PI * 2;
+
+      const xTip = this.X + Math.cos(angle) * (this.Radius + spikeLength);
+      const yTip = this.Y + Math.sin(angle) * (this.Radius + spikeLength);
+
+      const perpAngle = angle + Math.PI / 2;
+
+      const xBase1 = this.X + Math.cos(angle) * this.Radius + Math.cos(perpAngle) * baseWidth;
+      const yBase1 = this.Y + Math.sin(angle) * this.Radius + Math.sin(perpAngle) * baseWidth;
+
+      const xBase2 = this.X + Math.cos(angle) * this.Radius - Math.cos(perpAngle) * baseWidth;
+      const yBase2 = this.Y + Math.sin(angle) * this.Radius - Math.sin(perpAngle) * baseWidth;
+
+      ctx.beginPath();
+      ctx.moveTo(xBase1, yBase1);
+      ctx.lineTo(xTip, yTip);
+      ctx.lineTo(xBase2, yBase2);
+      ctx.closePath();
+      ctx.fillStyle = 'rgb(0, 180, 0)';
+      ctx.fill();
+    }
+
+    // Основной круг
+    ctx.beginPath();
+    ctx.arc(this.X, this.Y, this.Radius, 0, Math.PI * 2);
+    ctx.fillStyle = 'rgb(0, 200, 0)';
+    ctx.fill();
+    ctx.closePath();
+
+    // Внутренние шипы
+    const innerSpikeCount = 6;
+    const innerRadius = this.Radius * 0.5;
+    const innerSpikeLength = this.Radius * 0.3;
+    const innerBaseWidth = innerRadius * 0.2;
+    for (let i = 0; i < innerSpikeCount; i++) {
+      const angle = (i / innerSpikeCount) * Math.PI * 2;
+
+      const xTip = this.X + Math.cos(angle) * (innerRadius + innerSpikeLength);
+      const yTip = this.Y + Math.sin(angle) * (innerRadius + innerSpikeLength);
+
+      const perpAngle = angle + Math.PI / 2;
+
+      const xBase1 = this.X + Math.cos(angle) * innerRadius + Math.cos(perpAngle) * innerBaseWidth;
+      const yBase1 = this.Y + Math.sin(angle) * innerRadius + Math.sin(perpAngle) * innerBaseWidth;
+
+      const xBase2 = this.X + Math.cos(angle) * innerRadius - Math.cos(perpAngle) * innerBaseWidth;
+      const yBase2 = this.Y + Math.sin(angle) * innerRadius - Math.sin(perpAngle) * innerBaseWidth;
+
+      ctx.beginPath();
+      ctx.moveTo(xBase1, yBase1);
+      ctx.lineTo(xTip, yTip);
+      ctx.lineTo(xBase2, yBase2);
+      ctx.closePath();
+      ctx.fillStyle = 'rgba(0, 220, 0)';
+      ctx.fill();
+    }
+  }
 }
