172186cc2d7d7ec4ac85e6363d442b509160ae5d
Jkoroleva <ykoroleva@t-argos.ru>
2024-12-26 16:19:44 +0300

    fix(GAME-32): fix tests

--- packages/client/src/components/ErrorNotification/ErrorNotification.tsx
+++ packages/client/src/components/ErrorNotification/ErrorNotification.tsx
@@ -1,14 +1,12 @@
 import React, { ComponentType } from 'react';
 import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
 import styles from './ErrorNotification.module.scss';
+import { IErrorNotificationProps } from './ErrorNotification.interface';
 
 const FallbackComponent: ComponentType<FallbackProps> = ({ error }) => (
-  <div className={styles['notification-error']}>
-    <h1>Something went wrong</h1>
-    <p>{error.message}</p>
-  </div>
+  <div className={styles['notification-error']}>Something went wrong: {error.message}</div>
 );
 
-export const ErrorNotification = ({ children }: { children: React.ReactNode }) => (
+export const ErrorNotification = ({ children }: IErrorNotificationProps) => (
   <ErrorBoundary FallbackComponent={FallbackComponent}>{children}</ErrorBoundary>
 );
