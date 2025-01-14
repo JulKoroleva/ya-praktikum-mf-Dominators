148166f0c8348a8954352416e355afda4498d8ba
Lira <61969768+lira-bazh@users.noreply.github.com>
2025-01-14 18:21:34 +0300

    feat(GAME-33): add Pointer Lock API (#39)

--- packages/client/src/pages/Game/components/index.ts
+++ packages/client/src/pages/Game/components/index.ts
@@ -1,3 +1,4 @@
-export { CanvasComponent } from './CanvasComponent';
+export { CanvasComponent, MODAL_CONTENT } from './CanvasComponent';
 export { EndGame } from './EndGame';
 export { StartGame } from './StartGame';
+export { GoBackModalContent } from './GoBackModal';
--- packages/client/src/redux/requests/pagesRequests/index.ts
+++ packages/client/src/redux/requests/pagesRequests/index.ts
@@ -2,3 +2,4 @@ export { authorizationRequest, logoutRequest } from './authorizationRequests';
 export { registrationRequest } from './registrationRequests';
 export { profileRequests, avatarRequests, passwordRequests } from './profileRequests';
 export { fetchForum } from './forumRequests';
+export { addLeaderBoardEntry } from './leaderBoardRequest';
